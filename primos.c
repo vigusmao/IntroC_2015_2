db9e239051b163e4609606d95c7acf3e677cebee
Vinícius Gusmão <vigusmao@gmail.com>
2015-11-03 09:07:23 -0200

    Contando o numero de primos menores que um numero dado

--- primos.c
+++ primos.c
@@ -3,18 +3,26 @@
 #include<math.h>
 
 int eh_primo_1(int x) {
+    /* Testa divisibilidade por todos os inteiros
+       entre 2 e o antecessor de x
+    */
+
     int divisor = 2;
     while (divisor < x) {
         if (x % divisor == 0) {
-            return 0;
+            return 0;  // nao eh primo!
         } else {
             divisor++;
         }
     }
-    return 1;
+    return 1;  // eh primo!
 }
 
 int eh_primo_2(int x) {
+    /* Testa divisibilidade por todos os inteiros
+       entre 2 e a metade de x
+    */
+
     int divisor = 2;
     while (divisor <= x/2) {
         if (x % divisor == 0) {
@@ -27,6 +35,10 @@ int eh_primo_2(int x) {
 }
 
 int eh_primo_3(int x) {
+    /* Testa divisibilidade por todos os inteiros
+       entre 2 e a raiz quadrada de x
+     */
+
     int divisor = 2;
     while (divisor <= sqrt(x)) {
         if (x % divisor == 0) {
@@ -39,6 +51,10 @@ int eh_primo_3(int x) {
 }
 
 int eh_primo_4(int x) {
+    /* Testa divisibilidade por 2 e por todos os inteiros
+       impares entre 3 e a raiz quadrada de x
+    */
+
     if ((x != 2) &&
         (x % 2 == 0)) {
         return 0;
@@ -60,13 +76,17 @@ int main() {
     int limite;
     scanf("%d", &limite);
 
+    int contador_de_primos = 0;
     int numero = 2;
     while (numero <= limite) {
         if (eh_primo_1(numero) == 1) {
-            printf("\n%d", numero);
+            contador_de_primos++;
         }
         numero++;
     }
+
+    printf("\nExistem %d primos menores ou iguais a %d\n",
+           contador_de_primos, limite);
 
     return 0;
 }
