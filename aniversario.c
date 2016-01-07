0aab544540d30e9274b9cc63c2a50e14b1c7a17f
Vinícius Gusmão <vigusmao@gmail.com>
2016-01-07 09:44:05 -0200

    Paradoxo do Aniversario

---
+++ aniversario.c
@@ -0,0 +1,62 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+int sorteie(int a, int b) {
+    return rand() % (b-a+1) + a;
+}
+
+int simular_aniversarios_naive(int n_pessoas) {
+// retorna 1, se houver aniversarios coincidentes
+// retorna 0, caso contrario
+    int* aniversarios = (int*) malloc(n_pessoas * sizeof(int));
+    int i, j;
+    for (i=0; i<n_pessoas; i++) {
+        int aniversario = sorteie(1, 365);
+        aniversarios[i] = aniversario;
+    }
+    for (i=0; i<n_pessoas; i++) {
+        for (j=i+1; j<n_pessoas; j++) {
+            if (aniversarios[i] == aniversarios[j]) {
+                free(aniversarios);
+                return 1;  // Encontrei dois aniversarios iguais!
+            }
+        }
+    }
+    free(aniversarios);
+    return 0;  // Nao encontrei dois iguais!
+}
+
+int simular_aniversarios(int n_pessoas) {
+// retorna 1, se houver aniversarios coincidentes
+// retorna 0, caso contrario
+    int dias[366];
+    int i;
+    for (i=0; i<366; i++) {  // inicializando o array com zeros
+        dias[i] = 0;
+    }
+    for (i=0; i<n_pessoas; i++) {
+        int aniversario = sorteie(1, 365);
+        dias[aniversario]++;
+        if (dias[aniversario] == 2) {
+            return 1;  // Ja descobri que tem dois caras no mesmo dia!
+        }
+    }
+    return 0;  // Nao ha dois caras no mesmo dia!
+}
+
+int main() {
+    srand(time(NULL));
+
+    int repeticoes = 1000000;
+    int cont_sucessos = 0;
+    int i;
+    for (i=0; i<repeticoes; i++) {
+        cont_sucessos += simular_aniversarios_naive(57);
+    }
+    float prob = 1.0 * cont_sucessos / repeticoes;
+    printf("\nProbabilidade = %f", prob);
+
+    printf("\n");
+    return 0;
+}
